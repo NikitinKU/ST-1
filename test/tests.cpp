b418ceb4895a2c3567a0c0c6cf841cf149129e03
NikitinKU <90148254+NikitinKU@users.noreply.github.com>
2024-03-04 15:21:32 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,6 +1,36 @@
-
+// Copyright 2024 Kirill Nikitin
 #include <gtest/gtest.h>
 #include <cstdint>
 #include "alg.h"
 
+TEST(CheckPrimeTest, SmallValues) {
+  EXPECT_FALSE(checkPrime(0));
+  EXPECT_FALSE(checkPrime(1));
+  EXPECT_TRUE(checkPrime(2));
+  EXPECT_TRUE(checkPrime(3));
+  EXPECT_FALSE(checkPrime(4));
+  EXPECT_TRUE(checkPrime(5));
+  // Добавить дополнительные тестовые случаи по вашему усмотрению
+}
 
+TEST(NPrimeTest, SmallValues) {
+  EXPECT_EQ(nPrime(1), 2);
+  EXPECT_EQ(nPrime(2), 3);
+  EXPECT_EQ(nPrime(3), 5);
+  EXPECT_EQ(nPrime(4), 7);
+  // Добавить дополнительные тестовые случаи по вашему усмотрению
+}
+
+TEST(NextPrimeTest, SmallValues) {
+  EXPECT_EQ(nextPrime(2), 3);
+  EXPECT_EQ(nextPrime(3), 5);
+  EXPECT_EQ(nextPrime(5), 7);
+  EXPECT_EQ(nextPrime(7), 11);
+  // Добавить дополнительные тестовые случаи по вашему усмотрению
+}
+
+TEST(SumPrimeTest, SmallValues) {
+  EXPECT_EQ(sumPrime(10), 17);
+  EXPECT_EQ(sumPrime(20), 77);
+  // Добавить дополнительные тестовые случаи по вашему усмотрению
+}
